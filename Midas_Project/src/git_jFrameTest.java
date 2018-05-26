f7c0f11e293870999963a53d24b37f77c579c190
jypark <ghysnzsy@naver.com>
2018-05-26 10:04:17 +0900

    jFrameTest
    jFrameTest + Window Builder Test

---
+++ Midas_Project/src/git_jFrameTest.java
@@ -0,0 +1,47 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+public class git_jFrameTest extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtTest;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					git_jFrameTest frame = new git_jFrameTest();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public git_jFrameTest() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		txtTest = new JTextField();
+		txtTest.setText("test");
+		contentPane.add(txtTest, BorderLayout.CENTER);
+		txtTest.setColumns(10);
+	}
+
+}
