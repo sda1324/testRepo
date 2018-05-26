0fe3dc2c498ce8b22073712f771299f3add50172
jypark <ghysnzsy@naver.com>
2018-05-26 19:43:42 +0900

    re Upload

---
+++ Midas_Project/src/MouseOverListener.java
@@ -0,0 +1,90 @@
+
+
+import javax.swing.JPanel;
+
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.Component;
+import java.awt.Cursor;
+
+public class MouseOverListener extends JPanel implements MouseListener {
+
+	MainFrame main;
+	Object source;
+	public MouseOverListener(Object t)//DefaultMap main)
+	{
+		main = MainFrame.getInstance();
+		source = t;
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if(e.isPopupTrigger()) 
+		{
+			//0: blank
+			//1: wall
+			//2: point
+			//3: furniture
+			//4: window
+			//5: door
+			PopupMenuWindow popupT = new PopupMenuWindow(source);
+			popupT.SetItem(source.getClass().getSimpleName());
+			popupT.show(e.getX(), e.getY());
+			System.out.println("here");
+		}
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		Cursor cursor;
+		//System.out.println(e.getSource().getClass().getName());
+
+		//String t = e.getSource().getClass().getName().toString();
+		
+		System.out.println(source.getClass().getSimpleName());
+		//System.out.println(text[0]);
+		//System.out.println(((Component) e.getSource()).getName());
+		switch(source.getClass().getSimpleName())
+		{
+		case "Wall":
+			//cursor = new Cursor(Cursor.CROSSHAIR_CURSOR);
+			//main.setCursor(cursor);
+			//main.setCursor(ERROR);
+			break;
+		case "Point":
+
+			//cursor = new Cursor(Cursor.CROSSHAIR_CURSOR);
+			//main.setCursor(cursor);
+			break;
+		default:
+			//cursor = new Cursor(Cursor.CROSSHAIR_CURSOR);
+			//main.setCursor(cursor);
+			break;
+		}
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
